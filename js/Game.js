cd5dec67b2b5a2054965a8246c3435afbeb10103
Tulip-tanwar <amittanwar75@gmai.com>
2021-01-30 18:58:43 +0530

    fourPlayers

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,39 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
       form = new Form()
       form.display();
+    }
+  }
+
+  play(){
+    form.hide();
+    text("Game Start",120,100);
+    player.getPlayerInfo();
+    if(allPlayers !==undefined){
+      var displayPosition=130;
+      for(var plr in allPlayers){
+        if(plr==="player"+player.index)
+        fill("red")
+        else
+        fill("black");
+        displayPosition+=20;
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,displayPosition)
+      }
+     
+    }
+
+    if(keyIsDown(UP_ARROW)&& player.index!==null){
+      player.distance+=50
+      player.update();
     }
   }
 }
